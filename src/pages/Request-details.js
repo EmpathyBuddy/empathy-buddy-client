27583233a7d2b07e29370b3c8615d4f59c3217c6
Emilio Moreno <emiliomoremil@gmail.com>
2023-06-30 13:55:32 +0200

    request details view is showing info

--- src/pages/Request-details.js
+++ src/pages/Request-details.js
@@ -5,26 +5,25 @@ import { useState, useEffect } from "react";
 const API_URL = "http://localhost:5005";        // <== ADD
 
 
-function RequestDetails (){
+function RequestDetails (props){
     const [request, setRequest] = useState(null);
 
     const {requestId} = useParams()
     // console.log(requestId)
 
     const getRequest = () => {
-        axios
-      .get(
-        `${API_URL}/api/requests/${requestId}`,
-        // { headers: { Authorization: `Bearer ${storedToken}` } }
-      )
+      axios
+        .get(
+          `${API_URL}/api/requests/${requestId}`
+          // { headers: { Authorization: `Bearer ${storedToken}` } }
+        )
 
-      .then((response) => {
-        const oneRequest = response.data;
-        setRequest(oneRequest);
-      })
-      .catch((error) => console.log(error));
-
-    }
+        .then((response) => {
+          const oneRequest = response.data;
+          setRequest(oneRequest);
+        })
+        .catch((error) => console.log(error));
+    };
 
 
     useEffect(()=> {                   // <== ADD AN EFFECT
@@ -36,7 +35,14 @@ function RequestDetails (){
 
 return(
 <div>
-<p>Hello worldest</p>
+    <h2>{request.feeling}</h2>
+    <p>{request.date}</p>
+    <p>{request.time}</p>
+    <p>{request.phone}</p>
+    <p>{request.language}</p>
+   
+
+
 
 </div>
 )
