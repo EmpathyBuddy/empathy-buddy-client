56c85cb436beffb7e94a2706ed683e3461c6888a
Emilio Moreno <emiliomoremil@gmail.com>
2023-07-03 19:37:23 +0200

    guidelines page completed

--- src/pages/Guidelines.js
+++ src/pages/Guidelines.js
@@ -1,8 +1,56 @@
 function Guidelines(){
     return(
         <div>
+            <h2>
             Buddy up! <br />
-            start giving empathy!
+            Understand some basics before you start giving empathy.
+            </h2>
+
+            <h3>What supports someone requesting emapthy?</h3>
+            <ul>
+                <li>
+                Empathy means holding PRESENCE with as close to 100% attention as possible.
+                </li>
+                <li>
+                The listener is aware that no matter what the speaker is saying, the listener does not believe or disbelieve the story. 
+                </li>
+                <li>
+                Agree on a time limit that feels comfortable for both.
+                </li>
+                <li>
+                Only at the end of the agreed upon time, the speaker may ask for advise, education, reassurance, sympathy, etc. and the Empathy Buddy can respond if they are moved to.
+                </li>
+                <li>
+                Agree that during our empathy time each of us will hold the intention of not responding from one of the empathy blockers you will find below.
+                </li>
+            </ul>
+            
+            
+            
+
+            <h3>What blocks empathy?</h3>
+            <ul>
+                <li>When a listener starts back and forth talking</li>
+                <li>Judging: "That's wrong! / You can't do that!"</li>
+                <li>Educating: "This could turn into a positive experience if you just...."</li>
+                <li>Advising: "I think you should..."</li>
+                <li>One-Upping: "Thats nothing, wait until you hear what happened to my cousing..."</li>
+                <li>Consoling: "It wasn't your fault: you did hte best you could do..."</li>
+                <li>Sympathizing: "Oh, you poor thing...!"</li>
+                <li>Storytelling: " This reminds me of somethig I heard on the news..."</li>
+                <li>Shutting down: "Oh, Cheer up. Dont feel so bad!" "Come on, lets go for an ide cream"</li>
+                <li>Interrogating: "When did this begin?"</li>
+                <li>Correcting: "Thats not how it happened."</li>
+                <li>Reassuring: "It will all be OK."</li>
+                <li>Denial of feelings: "Dont be silly, dont worry about this. It makes no sense to worry about this."</li>
+                <li>Minimizing: "You are drowning in a glass of water! This is really not so important"</li>
+                <li>Diagnosing: " Your problem is that you are a compulsive worrier."</li>
+                <li>Analyzing: "I thik you are reacting like this because of your relation with your mum"</li>
+           
+           
+            </ul>
+
+            
         </div>
     )
 }
